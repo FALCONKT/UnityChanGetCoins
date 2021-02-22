3c67aa1bb920de0f2ee15131ff14166a92f4b2df
Ubuntu <ubuntu@ip-172-31-13-136.ec2.internal>
2021-02-22 00:23:40 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,11 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Unityちゃんと不思議な小判</h1>
+            
+            
+             {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'LogInしてPlayする！！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+
         </div>
     </div>
 @endsection
