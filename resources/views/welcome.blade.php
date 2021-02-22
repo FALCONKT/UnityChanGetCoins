3155983cf18f6b13d97ce0aae9a736f711dd8036
Ubuntu <ubuntu@ip-172-31-13-136.ec2.internal>
2021-02-22 00:37:40 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,19 @@
 
 @section('content')
     <div class="center jumbotron">
+
         <div class="text-center">
-            <h1>Unityちゃんと不思議な小判</h1>
-            
-            
-             {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'LogInしてPlayする！！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+
+            @if (Auth::check())
+            {{ Auth::user()->name }}
+            @else
+
+                <h1>Unityちゃんと不思議な小判</h1>
+
+                 {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'LogInしてPlayする！！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+
+            @endif
 
         </div>
     </div>
